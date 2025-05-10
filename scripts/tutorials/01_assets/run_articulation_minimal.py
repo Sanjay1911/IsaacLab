30e309c7fd36bfa743a0ed91159be3ccdd870104
Sanjayyadav <sanjayyadav.tamilselvam@tuhh.de>
2025-05-10 16:51:46 +0200

    WIP

--- scripts/tutorials/01_assets/run_articulation_minimal.py
+++ scripts/tutorials/01_assets/run_articulation_minimal.py
@@ -33,7 +33,10 @@ simulation_app = app_launcher.app
 """Rest everything follows."""
 
 import torch
-
+import numpy as np
+from pxr import UsdGeom, UsdPhysics
+import omni.log
+import omni.physics.tensors.impl.api as physx
 import isaacsim.core.utils.prims as prim_utils
 
 import isaaclab.sim as sim_utils
@@ -46,7 +49,7 @@ from isaaclab.managers import SceneEntityCfg
 from isaaclab.utils.assets import ISAAC_NUCLEUS_DIR
 from isaaclab.markers import VisualizationMarkers
 from isaaclab.markers.config import FRAME_MARKER_CFG
-
+from isaaclab.utils.warp import convert_to_warp_mesh, raycast_mesh
 ##
 # Pre-defined configs
 ##
@@ -70,7 +73,18 @@ class MinimalSceneCfg(InteractiveSceneCfg):
             usd_path=f"{ISAAC_NUCLEUS_DIR}/Props/Mounts/Stand/stand_instanceable.usd", scale=(2.0, 2.0, 2.0)
         ),
     )
+
+    skull = AssetBaseCfg(
+        prim_path="{ENV_REGEX_NS}/Skull",
+        spawn=sim_utils.MeshFileCfg(
+            file_path="/home/sanjay/thesis_replications/curobo_thesis_fork/src/curobo/content/assets/scene/skull.obj"
+        ),
+        init_state=AssetBaseCfg.InitialStateCfg(pos=(0.0, 0.0, 0.50)),
+    )
     robot = UR5_CFG.replace(prim_path="{ENV_REGEX_NS}/Robot")
+
+def sample_points_skull():
+    pass
 
 def main():
     """Main function."""
@@ -79,15 +93,41 @@ def main():
     sim = sim_utils.SimulationContext(sim_cfg)
     # Set main camera
     sim.set_camera_view([2.0, 1.0, 2.0], [0.0, 0.0, 0.5])
-    scene_cfg = MinimalSceneCfg(num_envs=32, env_spacing=2.0)
+    scene_cfg = MinimalSceneCfg(num_envs=4, env_spacing=2.0)
     scene = InteractiveScene(scene_cfg)
     sim.reset()
 
     robot = scene["robot"]
+    skull = scene["skull"]
+    skull_mesh_prim = sim_utils.find_first_matching_prim(prim_path_regex="/World/envs/env_.*/Skull")
+    print("[INFO] Mesh Prim: ", skull_mesh_prim, skull_mesh_prim.GetTypeName())
+    print("[INFO] Mesh Prim Children: ", skull_mesh_prim.GetAllChildren())
+    mesh_prim = sim_utils.get_first_matching_child_prim(
+        str(skull_mesh_prim.GetPath()), lambda prim: prim.GetTypeName() == "Mesh"
+    )
 
+    if mesh_prim is None or not mesh_prim.IsValid():
+        raise RuntimeError(f"Invalid mesh prim at path: {skull_mesh_prim.GetPath()}")
+
+    try:
+        mesh_prim = UsdGeom.Mesh(mesh_prim)
+        points = np.asarray(mesh_prim.GetPointsAttr().Get())
+        transform_matrix = np.array(omni.usd.get_world_transform_matrix(mesh_prim)).T
+        points = np.matmul(points, transform_matrix[:3, :3].T)
+        points += transform_matrix[:3, 3]
+        indices = np.asarray(mesh_prim.GetFaceVertexIndicesAttr().Get())
+        wp_mesh = convert_to_warp_mesh(points, indices, device=sim.device)
+        if wp_mesh is not None: 
+            print("[INFO] Converted to warp mesh")
+    except Exception as e:
+        print("[ERROR] Failed to convert to mesh prim due to : ", e)
+        pass
+
+    # print("[INFO] Table: ", table.get_local_poses(), table.get_world_poses())
+    # print("[INFO]: SKULL: ", skull.get_local_poses(), skull.get_world_poses())
     robot_entity_cfg = SceneEntityCfg("robot", joint_names=[".*"], body_names=["tooltip"])
     robot_entity_cfg.resolve(scene)
-    print("[INFO]: Setup complete...")
+    #print("[INFO]: Setup complete...")
 
     sim_dt = sim.get_physics_dt()
     count = 0
@@ -102,7 +142,6 @@ def main():
             # we offset the root state by the origin since the states are written in simulation world frame
             # if this is not done, then the robots will be spawned at the (0, 0, 0) of the simulation world
             root_state = robot.data.default_root_state.clone()
-            print("[INFO]: Root state: ", root_state)
             # Tilt 90° about X-axis => quaternion: [w, x, y, z] = [cos(θ/2), sin(θ/2)*axis]
             q_tilt = torch.tensor([[0.7071, 0.7071, 0.0, 0.0]], device=sim.device)  # 90° around X
             # tilt with some noise
@@ -111,13 +150,13 @@ def main():
 
             root_state[:, :3] = scene.env_origins # set root position
             root_state[:, 3:7] = q_tilt  # set root orientation
-            print("[INFO]: Updated Root state: ", root_state)
+            #print("[INFO]: Updated Root state: ", root_state)
             robot.write_root_pose_to_sim(root_state[:, :7])
             robot.write_root_velocity_to_sim(root_state[:, 7:])
  
             joint_pos, joint_vel = robot.data.default_joint_pos.clone(), robot.data.default_joint_vel.clone()
             joint_pos += torch.rand_like(joint_pos) * 0.05
-            print("[INFO]: Joint pos: ", joint_pos)
+            #print("[INFO]: Joint pos: ", joint_pos)
             robot.write_joint_state_to_sim(joint_pos, joint_vel)
 
             robot.reset()
