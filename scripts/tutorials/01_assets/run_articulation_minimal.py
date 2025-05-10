1c74ccb404d8fa106827b89fd294bcc11b1cc763
Sanjayyadav <sanjayyadav.tamilselvam@tuhh.de>
2025-05-10 14:10:18 +0200

    working with robot - add preop

--- scripts/tutorials/01_assets/run_articulation_minimal.py
+++ scripts/tutorials/01_assets/run_articulation_minimal.py
@@ -37,56 +37,64 @@ import torch
 import isaacsim.core.utils.prims as prim_utils
 
 import isaaclab.sim as sim_utils
-from isaaclab.assets import Articulation
-from isaaclab.sim import SimulationContext
+from isaaclab.scene import InteractiveScene
+from isaaclab_assets import UR5N_CFG, UR5H_CFG, FRANKA_PANDA_CFG, UR10_CFG
+from isaaclab.utils import configclass
+from isaaclab.scene import InteractiveSceneCfg
+from isaaclab.assets import AssetBaseCfg
+from isaaclab.managers import SceneEntityCfg
+from isaaclab.utils.assets import ISAAC_NUCLEUS_DIR
+from isaaclab.markers import VisualizationMarkers
+from isaaclab.markers.config import FRAME_MARKER_CFG
 
 ##
 # Pre-defined configs
 ##
 from isaaclab_assets import CARTPOLE_CFG, UR5_CFG  # isort:skip
 
+@configclass
+class MinimalSceneCfg(InteractiveSceneCfg):
+    ground = AssetBaseCfg(
+        prim_path="/World/ground",
+        spawn=sim_utils.GroundPlaneCfg(),
+        init_state=AssetBaseCfg.InitialStateCfg(pos=(0.0, 0.0, -1.05)),
+    )
 
-def design_scene() -> tuple[dict, list[list[float]]]:
-    """Designs the scene."""
-    # Ground-plane
-    cfg = sim_utils.GroundPlaneCfg()
-    cfg.func("/World/defaultGroundPlane", cfg)
-    # Lights
-    cfg = sim_utils.DomeLightCfg(intensity=3000.0, color=(0.75, 0.75, 0.75))
-    cfg.func("/World/Light", cfg)
+    dome_light = AssetBaseCfg(
+        prim_path="/World/Light", spawn=sim_utils.DomeLightCfg(intensity=3000.0, color=(0.75, 0.75, 0.75))
+    )
 
-    # Create separate groups called "Origin1", "Origin2"
-    # Each group will have a robot in it
-    origins = [[0.50, 0.50, 0.50], [-1.0, 0.0, 0.50]]
-    # Origin 1
-    prim_utils.create_prim("/World/Origin1", "Xform", translation=origins[0])
-    # Origin 2
-    prim_utils.create_prim("/World/Origin2", "Xform", translation=origins[1])
+    table = AssetBaseCfg(
+        prim_path="{ENV_REGEX_NS}/Table",
+        spawn=sim_utils.UsdFileCfg(
+            usd_path=f"{ISAAC_NUCLEUS_DIR}/Props/Mounts/Stand/stand_instanceable.usd", scale=(2.0, 2.0, 2.0)
+        ),
+    )
+    robot = UR5_CFG.replace(prim_path="{ENV_REGEX_NS}/Robot")
 
-    # Articulation
-    ur5_cfg = UR5_CFG.copy()
-    ur5_cfg.prim_path = "/World/Origin.*/Robot"
-    ur5 = Articulation(cfg=ur5_cfg)
+def main():
+    """Main function."""
+    # Load kit helper
+    sim_cfg = sim_utils.SimulationCfg(dt=0.01, device=args_cli.device)
+    sim = sim_utils.SimulationContext(sim_cfg)
+    # Set main camera
+    sim.set_camera_view([2.0, 1.0, 2.0], [0.0, 0.0, 0.5])
+    scene_cfg = MinimalSceneCfg(num_envs=32, env_spacing=2.0)
+    scene = InteractiveScene(scene_cfg)
+    sim.reset()
 
-    # return the scene information
-    scene_entities = {"ur5": ur5}
-    return scene_entities, origins
+    robot = scene["robot"]
 
+    robot_entity_cfg = SceneEntityCfg("robot", joint_names=[".*"], body_names=["tooltip"])
+    robot_entity_cfg.resolve(scene)
+    print("[INFO]: Setup complete...")
 
-def run_simulator(sim: sim_utils.SimulationContext, entities: dict[str, Articulation], origins: torch.Tensor):
-    """Runs the simulation loop."""
-    # Extract scene entities
-    # note: we only do this here for readability. In general, it is better to access the entities directly from
-    #   the dictionary. This dictionary is replaced by the InteractiveScene class in the next tutorial.
-    robot = entities["ur5"]
-    print("Joints: ", robot.joint_names)
-    # Define simulation stepping
     sim_dt = sim.get_physics_dt()
     count = 0
     # Simulation loop
     while simulation_app.is_running():
         # Reset
-        if count % 100 == 0:
+        if count % 150 == 0:
             # reset counter
             count = 0
             # reset the scene entities
@@ -98,53 +106,29 @@ def run_simulator(sim: sim_utils.SimulationContext, entities: dict[str, Articula
             # Tilt 90° about X-axis => quaternion: [w, x, y, z] = [cos(θ/2), sin(θ/2)*axis]
             q_tilt = torch.tensor([[0.7071, 0.7071, 0.0, 0.0]], device=sim.device)  # 90° around X
             # tilt with some noise
-            q_tilt += torch.rand_like(q_tilt) * 0.05
+            q_tilt += torch.rand_like(q_tilt) * 0.5
             q_tilt = q_tilt / q_tilt.norm(dim=1, keepdim=True)  # normalize
 
-            root_state[:, :3] = origins  # set root position
+            root_state[:, :3] = scene.env_origins # set root position
             root_state[:, 3:7] = q_tilt  # set root orientation
-            robot.write_root_pose_to_sim(root_state[:, :7])
             print("[INFO]: Updated Root state: ", root_state)
             robot.write_root_pose_to_sim(root_state[:, :7])
             robot.write_root_velocity_to_sim(root_state[:, 7:])
-            print("[INFO]: Writing root state to sim...")
-            # set joint positions with some noise
+ 
             joint_pos, joint_vel = robot.data.default_joint_pos.clone(), robot.data.default_joint_vel.clone()
             joint_pos += torch.rand_like(joint_pos) * 0.05
             print("[INFO]: Joint pos: ", joint_pos)
             robot.write_joint_state_to_sim(joint_pos, joint_vel)
-            # clear internal buffers
+
             robot.reset()
             print("[INFO]: Resetting robot state...")
-        robot.write_data_to_sim()
-        # Perform step
+        scene.write_data_to_sim()
         sim.step()
-        # Increment counter
         count += 1
-        # Update buffers
-        robot.update(sim_dt)
-
-
-def main():
-    """Main function."""
-    # Load kit helper
-    sim_cfg = sim_utils.SimulationCfg(device=args_cli.device)
-    sim = SimulationContext(sim_cfg)
-    # Set main camera
-    sim.set_camera_view([2.5, 0.0, 4.0], [0.0, 0.0, 2.0])
-    # Design scene
-    scene_entities, scene_origins = design_scene()
-    scene_origins = torch.tensor(scene_origins, device=sim.device)
-    # Play the simulator
-    sim.reset()
-    # Now we are ready!
-    print("[INFO]: Setup complete...")
-    # Run the simulator
-    run_simulator(sim, scene_entities, scene_origins)
+        scene.update(sim_dt)
 
 
 if __name__ == "__main__":
-    # run the main function
+
     main()
-    # close sim app
     simulation_app.close()
